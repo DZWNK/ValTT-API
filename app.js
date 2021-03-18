e3decb22bf45d584f3477579a102263890bc8ec3
Mohammed Suleiman Mohamed Al-falahy <32552843+mamdinho@users.noreply.github.com>
2021-03-18 00:47:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
         }
     })
 })
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 userData.initialize().then(() => {
     eventData.initialize().then(() => {
